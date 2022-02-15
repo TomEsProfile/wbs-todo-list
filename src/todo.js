cd2b48ec6b23ed53d9c3957168a86e66944c6a22
Thomas <schmiddla@gmail.com>
2022-02-15 17:31:53 +0100

    merge

--- src/todo.js
+++ src/todo.js
@@ -1,32 +1,32 @@
-console.log('todo list!')
+console.log("todo list!");
 
-window.addEventListener("load", function(event) {
-    console.log('seite fertig geladen');
+window.addEventListener("load", function (event) {
+  console.log("seite fertig geladen");
 
-    // ... ab hier dann die Seite aufbauen mit den Daten aus der LocalStorage
-    // getListTypes().forEach(list => {
-    //     const div = createListDiv()
-    //     div.className = ''
+  // ... ab hier dann die Seite aufbauen mit den Daten aus der LocalStorage
+  // getListTypes().forEach(list => {
+  //     const div = createListDiv()
+  //     div.className = ''
 
-    //     this.document.getElementById('sidebar').appendChild(div)
-    // })
-})
+  //     this.document.getElementById('sidebar').appendChild(div)
+  // })
+});
 
-if(globalTodoList.length < 3) {
-    addTodo(createTodo('haushalt', 'Saugen'))
+if (globalTodoList.length < 3) {
+  addTodo(createTodo("haushalt", "Saugen"));
 }
 
 console.log(globalTodoList);
 
-const buttonAddList = document.getElementById("buttonAddList")
+const buttonAddList = document.getElementById("buttonAddList");
 
-function addList (event) {
-    const lists = getListTypes();
-    console.log(lists); 
-    lists.forEach( (list) => {
-     console.log(list.type)
-     console.log(list.count)
-    })
+function addList(event) {
+  const lists = getListTypes();
+  console.log(lists);
+  lists.forEach((list) => {
+    console.log(list.type);
+    console.log(list.count);
+  });
 }
 
 buttonAddList.addEventListener("click", addList);
