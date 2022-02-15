a37bf41faaf3921e3b7bc71943e71bbc6706280f
Thomas <schmiddla@gmail.com>
2022-02-15 17:31:01 +0100

    site loaded event

--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,17 @@
 console.log('todo list!')
 
+window.addEventListener("load", function(event) {
+    console.log('seite fertig geladen');
+
+    // ... ab hier dann die Seite aufbauen mit den Daten aus der LocalStorage
+    // getListTypes().forEach(list => {
+    //     const div = createListDiv()
+    //     div.className = ''
+
+    //     this.document.getElementById('sidebar').appendChild(div)
+    // })
+})
+
 if(globalTodoList.length < 3) {
     addTodo(createTodo('haushalt', 'Saugen'))
 }
